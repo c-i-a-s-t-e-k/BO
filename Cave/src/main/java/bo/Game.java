875f7772c5d26cfc02a638adf2176b3d76e49a70
IwoSzczepaniak <iszczepaniak@student.agh.edu.pl>
2024-05-13 19:42:01 +0200

    small_refactor

--- Cave/src/main/java/bo/Game.java
+++ Cave/src/main/java/bo/Game.java
@@ -3,20 +3,21 @@ package bo;
 import bo.cave.CaveMap;
 import bo.cave.enums.Direction;
 import bo.cave.enums.TileType;
+import bo.player.Backpack;
 import bo.player.Player;
 import bo.player.ResourceType;
 import org.javatuples.Pair;
 
+import javax.swing.text.Position;
 import java.util.*;
 
 import static java.lang.Integer.min;
-
 
 public class Game {
     CaveMap map;
     Player player;
     GamePath path;
-    int turnsLimit = Constants.TURNS_LIMIT;
+    int turnsLimit = 20;
 
     public Game(CaveMap map, Player player) {
         this.map = map;
@@ -31,15 +32,15 @@ public class Game {
         Queue<Pair<Player, Integer>> queue = new ArrayDeque<>();
         queue.add(new Pair<>(new Player(player), 0));
 
-        while (!queue.isEmpty()) {
+        while (!queue.isEmpty()){
             Pair<Player, Integer> currentPair = queue.poll();
             Player currentPlayer = currentPair.getValue0();
             Integer prevCost = currentPair.getValue1();
 
-            for (Pair<Direction, TileType> neighMove : currentPlayer.getMoves()) {
+            for (Pair<Direction, TileType> neighMove : currentPlayer.getMoves()){
                 Player neighbour = new Player(currentPlayer);
                 neighbour.makeMove(neighMove);
-                if (neighbour.getPosition().equals(base)) {
+                if (neighbour.getPosition().equals(base)){
                     return prevCost + 1;
                 }
                 if (!visited.contains(neighbour.getPosition())) {
@@ -56,22 +57,24 @@ public class Game {
         Set<Pair<Integer, Integer>> visited = new HashSet<>();
         Queue<Pair<Player, Integer>> queue = new ArrayDeque<>();
         queue.add(new Pair<>(new Player(player), 0));
+        // choose random destination cell within energyToGetBack
 
-        while (!queue.isEmpty()) {
+        while (!queue.isEmpty()){
             Pair<Player, Integer> qp = queue.poll();
             Player currentPlayer = qp.getValue0();
             int currentMoveNumber = qp.getValue1() + 1;
-            for (Pair<Direction, TileType> neighMove : currentPlayer.getMoves()) {
+            for (Pair<Direction, TileType> neighMove : currentPlayer.getMoves()){
                 Player neighbour = new Player(currentPlayer);
                 neighbour.makeMove(neighMove);
 
-                if (!visited.contains(neighbour.getPosition()) && neighbour.energyLeft() > calculateCostToBase(neighbour)) {
+                if (!visited.contains(neighbour.getPosition()) && neighbour.energyLeft() > calculateCostToBase(neighbour)){
                     visited.add(neighbour.getPosition());
                     queue.add(new Pair<>(neighbour, currentMoveNumber));
-                    if (currentMoveNumber % Constants.MOVES_PER_TURN == 0) {
+                    if (currentMoveNumber % 5 == 0) {
                         neighbour.use(ResourceType.FOOD);
                     }
-                } else if (neighbour.energyLeft() < calculateCostToBase(neighbour)) {
+                }
+                else if (neighbour.energyLeft() < calculateCostToBase(neighbour)){
                     return currentPlayer.getPosition();
                 }
             }
@@ -80,23 +83,26 @@ public class Game {
         return map.getBasePosition();
     }
 
-    List<Pair<Direction, TileType>> getMoves(Pair<Integer, Integer> destination) {
+    List<Pair<Direction, TileType>> getMoves(Pair<Integer, Integer> destination){
+        // BFS
         Set<Pair<Integer, Integer>> visited = new HashSet<>();
         Queue<Pair<Player, List<Pair<Direction, TileType>>>> queue = new ArrayDeque<>();
         queue.add(new Pair<>(new Player(player), new ArrayList<>()));
+        // choose random destination cell within energyToGetBack
 
-        while (!queue.isEmpty()) {
+        while (!queue.isEmpty()){
             Pair<Player, List<Pair<Direction, TileType>>> currentPair = queue.poll();
             Player currentPlayer = currentPair.getValue0();
             List<Pair<Direction, TileType>> currentList = new ArrayList<>(currentPair.getValue1());
 
-            for (Pair<Direction, TileType> neighMove : currentPlayer.getMoves()) {
+            for (Pair<Direction, TileType> neighMove : currentPlayer.getMoves()){
                 Player neighbour = new Player(currentPlayer);
                 neighbour.makeMove(neighMove);
-                if (neighbour.getPosition().equals(destination)) {
+                if (neighbour.getPosition().equals(destination)){
                     currentList.add(neighMove);
                     return currentList;
-                } else if (!visited.contains(neighbour.getPosition())) {
+                }
+                else if (!visited.contains(neighbour.getPosition())) {
                     visited.add(neighbour.getPosition());
                     List<Pair<Direction, TileType>> currentListCopy = new ArrayList<>(currentList);
                     currentListCopy.add(neighMove);
@@ -110,24 +116,34 @@ public class Game {
     public void startGame() {
         Pair<Integer, Integer> base = map.getBasePosition();
         path.addToPath(player);
+        int turnsDone = 0;
+//        int score = 0;
+        Pair<Integer, Integer> destination;
 
-
-        for (int turnsDone = 0; turnsDone < turnsLimit; ++turnsDone) {
-
-            Pair<Integer, Integer> destination = calculateRandomDestination();
+        destination = calculateRandomDestination();
+        while (turnsDone < turnsLimit){
+            System.out.println("TURN " + turnsDone);
+            System.out.println("DESTINATION  " + destination);
             List<Pair<Direction, TileType>> moves = getMoves(destination);
 
-            for (int move_i = 0; move_i < min(Constants.MOVES_PER_TURN, moves.size()); ++move_i) {
+            for (int move_i = 0; move_i < min(5, moves.size()); ++move_i) {
+
+                System.out.println(" p: " + player.getPosition());
+
                 if (player.getPosition().equals(base)) {
                     player.restockBackpack();
+                    destination = calculateRandomDestination();
                 } else if (player.getPosition().equals(destination)) {
                     destination = base;
                     moves = getMoves(destination);
                 }
 
-                player.makeMove(moves.get(move_i));
+                Pair<Direction, TileType> move = moves.get(move_i);
+                player.makeMove(move);
                 path.addToPath(player);
             }
+
+            turnsDone++;
             player.use(ResourceType.FOOD);
         }
     }
@@ -136,4 +152,5 @@ public class Game {
     public GamePath getAcceptablePath() {
         return path;
     }
+
 }
